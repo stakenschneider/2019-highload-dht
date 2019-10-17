17ede5f2f11747b391c9ef43de10572b623f7074
Маша <volkova.maria.d@gmail.com>
2019-10-17 21:33:45 +0300

    fix warning

--- src/main/java/ru/mail/polis/service/stakenschneider/NoSuchElementLite.java
+++ src/main/java/ru/mail/polis/service/stakenschneider/NoSuchElementLite.java
@@ -10,7 +10,7 @@ public class NoSuchElementLite extends NoSuchElementException {
     }
 
     @Override
-    public Throwable fillInStackTrace() {
+    public synchronized Throwable fillInStackTrace() {
             return this;
     }
 }
